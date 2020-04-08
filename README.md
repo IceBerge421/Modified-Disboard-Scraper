9e6dab434ac2ba9aaed34786d725dd5056e25eaf
Taven Argus <daegontaven@gmail.com>
2020-04-08 22:04:31 +0530

    Major Features Added
    - Supports UTF-8 for Windows
    - Shiny new progress bar
    - Displays top positional tags
    - Configuration moved out to a config file
    - Added documentation of config to README
    - Ability to disable output of CSV
    - More verbose in the console
    - Debug option for developers

---
+++ README.md
@@ -0,0 +1,53 @@
+# Disboard Scraper
+
+This script will scrape all the servers on a particular tag and show various statistics. It can also output the data into a file. This script should be used in moderation to prevent being detected as a DOS attack.
+
+These are the features currently available:
+
+- Display online member counts.
+- Display tags of each server.
+- Show most popular tags for each tag position.
+- Output server data as a file
+
+## Usage
+
+#### Requirements:
+- Make sure to download the latest [Python (>= 3.8.x)](https://www.python.org/downloads/).
+- Then install [pipenv](https://pypi.org/project/pipenv/).
+- Ensure you have [Git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git) installed
+
+Clone this repository:
+```
+git clone https://github.com/DiscordFederation/DisboardScraper.git
+```
+
+Then enter a pipenv environment using this command:
+
+```
+pipenv shell
+```
+
+Next install the requirements:
+
+```
+pipenv install
+```
+
+Finally edit the newly created `config.toml` file and run the script.
+
+```
+python app.py
+```
+
+## Configuration Options
+
+You should make a copy of the included `config.example.toml` file and save it as `config.toml` without your own settings for it to be picked up by the script.
+
+|  Configuration        |  Type            | Description                                                                                                                               |
+|-----------------------|------------------|-------------------------------------------------------------------------------------------------------------------------------------------|
+| `tag`                 | `str`            | This is the tag you want to analyze for servers.                                                                                          |
+| `pages`               | `int`            | This configures how many pages for a tag you want to scrape.                                                                              |
+| `top_positional_tags` | `true`,  `false` | This displays the most popular tags for each tag's position.                                                                              |
+| `tpt_limit`           | `int`            | Limits the number of tags shows for all tag positions. This only works if `top_positional_tags` is enabled.                               |
+| `csv`                 | `true`, `false`  | When enabled will output all scraped data into a CSV file. This should make further analysis easier.                                      |
+| `debug`               | `true`, `false`  | While set to `true` the script will display more output onto the console for debugging purposes. This will also disable the progress bar. |
--- app.py
+++ app.py
@@ -1,16 +1,64 @@
-import pandas as pd
+import sys
 from urllib.request import Request, urlopen
+
+import pandas as pd
+import toml
 from bs4 import BeautifulSoup
+from schema import Schema, Or, And, Use
+from tqdm import tqdm
 
-headers = {'User-Agent': 'Mozilla/5.0'}
-tag = "debate"
+try:
+    config = toml.load("config.toml")
+except FileNotFoundError:
+    print(
+        "No config file found. "
+        "Please define a config.toml as per the config.example.toml file."
+    )
+    sys.exit()
 
+# Config Schema Definition
+config_schema = Schema(
+    {
+        "bot": {
+            "tag": str,
+            "pages": int,
+            "top_positional_tags": Or(True, False),
+            "tpt_limit": And(Use(int), lambda n: n > 0),
+            "csv": Or(True, False),
+            "debug": Or(True, False),
+        }
+    },
+    ignore_extra_keys=True,
+)
+config_schema.validate(config)
+
+# Configurations
+tag = config['bot']['tag']
+pages = config['bot']['pages']
+top_positional_tags = config['bot']['top_positional_tags']
+tpt_limit = config['bot']['tpt_limit']
+csv = config['bot']['csv']
+debug = config['bot']['debug']
+
+# Constants
+HEADERS = {'User-Agent': 'Mozilla/5.0'}
+
+# Variables
 servers = []
-for page in range(1, 16):
+
+# Progress Bar
+print(f"Fetching {pages} Pages: \n")
+bar = tqdm(total=pages)
+
+# Iterate over each page for a tag
+for page in range(1, pages + 1):
     url = f"https://disboard.org/servers/tag/{tag}/{page}?sort=-member_count"
-    request = Request(url, headers=headers)
-    content = urlopen(request)
+    request = Request(url, headers=HEADERS)
+    resource = urlopen(request)
+    content = resource.read().decode(resource.headers.get_content_charset())
     soup = BeautifulSoup(content, 'html.parser')
+
+    # Iterate over each server
     for server_info in soup.find_all(class_='server-info'):
         server_name = server_info.find(class_="server-name")
         server_name_link = server_name.find('a')
@@ -21,16 +69,28 @@ for page in range(1, 16):
         for tag_ in parent.find_all(class_="tag"):
             tag_name = tag_.find(class_="name")
             tags.append(tag_name.contents[0].strip())
-        try:  # Dirty hack. Too lazy to use hasattr.
+
+        # Check servers without online counts
+        if hasattr(server_online, 'contents'):
             members_online_count = server_online.contents[0].strip()
-        except AttributeError as err_info:
+        else:
             members_online_count = "N/A"
+
+        # Create a server
         server = [
             server_name_link.contents[0].strip(),
             members_online_count
         ]
         server.extend(tags)
+
+        if debug:
+            print(f"Server: {server}")
+
+        # Add each server found
         servers.append(server)
+
+    # Increment Progress
+    bar.update(1)
 
 df = pd.DataFrame(
     servers,
@@ -44,5 +104,19 @@ df = pd.DataFrame(
         'Tag 5'
     ]
 )
-print("Creating output file...")
-df.to_csv('servers.csv', index=False, encoding='utf-8')
+
+# Close Progress Bar
+bar.close()
+
+if top_positional_tags:
+    print("\n== Top Tags by Position ==")
+    for i in range(1, 6):
+        print(f"Tag {i}: ")
+        print(f"{str(df[f'Tag {i}'].value_counts()[:tpt_limit])}\n")
+
+if csv:
+    print("Creating output file...")
+    df.to_csv(f'{tag}_servers.csv', index=False, encoding='utf-8-sig')
+    print(f"Done writing: {tag}_servers.csv")
+else:
+    print("Exiting without writing file!")
